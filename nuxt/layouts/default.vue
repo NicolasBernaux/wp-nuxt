17e5869ce29a7ae6c524e3623c1e98303212a0c6
Nicolas Bernaux <bernaux.n@gmail.com>
2020-03-06 10:20:22 +0100

    Update layout with external link

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <header>
+      <template v-for="link in menus['footermenu']">
+        <a
+          v-if="link.type_label === 'Custom Link'"
+          :key="link.ID"
+          :href="link.url"
+        >
+          {{ link.title }}
+        </a>
+        <nuxt-link v-else :key="link" :to="link.url">
+          {{ link.title }}
+        </nuxt-link>
+      </template>
+    </header>
     <nuxt />
   </div>
 </template>
